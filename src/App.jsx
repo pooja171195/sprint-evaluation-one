d79494d7c7edcc66e72de803e50fbf9ac13c3e96
pooja yadav <96077451+pooja171195@users.noreply.github.com>
2022-03-07 09:46:32 +0530

    bootstrap done

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,39 @@ function App() {
     <button className="remBook">
         -
     </button>
-    <span className="totalBooks">1</span>
+    <span className="totalBooks">13</span>
 </div>
 <div className="addItem">
-    <span>Books:</span>
+    <span>Penss:</span>
     <button className="addPen">
         +
     </button>
     <button className="remPen">
         -
     </button>
-    <span className="totalPen">1</span>
+    <span className="totalPen">10</span>
 </div>
-
+<div className="addItem">
+    <span>Notebooks:</span>
+    <button className="addNotebook">
+        +
+    </button>
+    <button className="remNotebook">
+        -
+    </button>
+    <span className="totalNotebook">44</span>
+</div>
+<div className="addItem">
+    <span>Inkpens:</span>
+    <button className="addInkpen">
+        +
+    </button>
+    <button className="remInkpen">
+        -
+    </button>
+    <span className="totalInkpen">78</span>
+</div>
+<div className="total">1</div>
    </>
   
   );
