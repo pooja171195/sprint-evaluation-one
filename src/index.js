f9431943fb8d18b9faff2e888fce415428659658
pooja yadav <96077451+pooja171195@users.noreply.github.com>
2022-03-07 10:57:45 +0530

    .

--- src/index.js
+++ src/index.js
@@ -5,27 +5,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './App.jsx';
 
-export function addBook(){
-  const [count,setCount]=useState(13)
-  return <div>
-    <button onClick={()=>{
-      setCount(count+1)
-    }}>Add</button>
-      <button onClick={()=>{
-        if(count>=1){
-          setCount(count-1)
-        }
+// export function addBook(){
+//   const [count,setCount]=useState(13)
+//   return <div>
+//     <button onClick={()=>{
+//       setCount(count+1)
+//     }}>Add</button>
+//       <button onClick={()=>{
+//         if(count>=1){
+//           setCount(count-1)
+//         }
       
-    }}>Subtract</button>
-      document.getElementById('addBook')
-  </div>
-}
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+//     }}>Subtract</button>
+//       document.getElementById('addBook')
+//   </div>
+// }
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
